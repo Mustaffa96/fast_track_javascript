0b0e87b941ddea2912223710acd1b668d6c61e15
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2023-08-14 00:46:30 +0800

    feat: add mouse_events

---
+++ 002_DOM_manipulation/08_mouse_events/app.js
@@ -0,0 +1,31 @@
+const clearBtn = document.querySelector(".clear-tasks");
+const card = document.querySelector(".card");
+const heading = document.querySelector("h5");
+
+// // Click
+// clearBtn.addEventListener('click', runEvent);
+// // Doubleclick
+// clearBtn.addEventListener('dbclick', runEvent);
+// // Mousedown
+// clearBtn.addEventListener('mousedown', runEvent);
+// // Mouseup
+// clearBtn.addEventListener('mouseup', runEvent);
+// // Mouseenter
+// clearBtn.addEventListener("mousenter", runEvent);
+// // Mouseleave
+// clearBtn.addEventListener("mousleave", runEvent);
+// // Mouseover
+// clearBtn.addEventListener("mousover", runEvent);
+// // Mouseout
+// clearBtn.addEventListener("mouseout", runEvent);
+// Mousemove
+card.addEventListener("mousemove", runEvent);
+
+// Event Handler
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
+
+  document.body.style.backgroundColor = `rgb( ${offsetX}, ${e.offsetY}, 40)`;
+}
