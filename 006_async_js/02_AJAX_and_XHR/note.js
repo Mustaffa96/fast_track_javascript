be2a3a1cbfe5c9add72757cadeb44bfbee1a9b7d
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2023-09-01 23:00:41 +0800

    feat: add notes for AJAX and XHR

---
+++ 006_async_js/02_AJAX_and_XHR/note.js
@@ -0,0 +1,15 @@
+/*AJAX (Asynchronous JavaScript and XML) and XHR (XMLHttpRequest) are both technologies used in web development to make asynchronous requests to a server from a web page without needing to reload the entire page. Here's a brief explanation of each:
+
+1. XHR (XMLHttpRequest):
+   - XHR is an API (Application Programming Interface) in JavaScript that allows you to send HTTP or HTTPS requests to a server and receive responses.
+   - It was the original technology used for making asynchronous requests in web applications, and it is still widely used today.
+   - XHR can retrieve data from a server in various formats, not just XML, despite its name. It can handle JSON, HTML, and other data formats as well.
+   - XHR uses callbacks or event listeners to handle the response from the server, making it asynchronous and non-blocking.
+
+2. AJAX (Asynchronous JavaScript and XML):
+   - AJAX is a broader concept that encompasses the use of XHR and other technologies to create interactive web applications.
+   - It allows web pages to make asynchronous requests to a server, retrieve data, and update parts of the page without requiring a full page refresh.
+   - Although the "X" in AJAX stands for XML, it is not limited to XML data and can work with various data formats, including JSON, plain text, and more.
+   - AJAX often involves combining XHR with JavaScript and HTML to create dynamic and responsive user interfaces.
+
+In summary, XHR is a specific JavaScript API for making asynchronous HTTP requests, while AJAX is a broader concept that includes various techniques and technologies for creating interactive web applications that can communicate with servers asynchronously. AJAX often involves using XHR, but it can also utilize other modern techniques like the Fetch API or libraries/frameworks like jQuery to achieve similar results.*/
